934994216db3ee1671ccbe5a9a7c6a3c68df13dd
RichardMadrigar <richard_madrigar99@hotmail.com>
2022-03-03 17:01:05 -0300

    att de tratamento de erro quando n achar arquivos

--- src/controllers/DeleteImgId.controller.ts
+++ src/controllers/DeleteImgId.controller.ts
@@ -13,23 +13,32 @@ export const DeleteImgId = async (req: Request, res: Response) => {
   try {
     const { key } = req.params;
 
-    const filePath = path.join(`${__dirname}, ../../../tmp/uploads/${key}`);
+    const SQL = 'DELETE FROM permissao_usuario WHERE key = $1';
 
-    const { rowCount } = await pool.query('DELETE FROM permissao_usuarios WHERE key = $1', [key]);
+    const { rowCount } = await pool.query(SQL, [key]);
 
     if (!rowCount) return res.status(400).json('Imagem não existe');
+
+    const filePath = path.join(`${__dirname}, ../../../tmp/uploads/${key}k`);
 
     if (process.env.STORAGE_TYPE === 's3') {
       const params = { Bucket: `${process.env.AWS_BUCKET}`, Key: key };
 
       await s3.deleteObject(params).promise();
     } else {
-      fs.promises.unlink(filePath);
+      await fs.promises.stat(`${filePath}${key}`)
+        .then(() => {
+          fs.promises.unlink(filePath);
+        })
+        .catch((error) => {
+          logger.fatal(error.message);
+          throw new Error(error);
+        });
     }
 
     return res.status(200).json('Img deletada com sucesso');
   } catch (error) {
     logger.fatal(error);
-    return res.status(500).json('Internal Server error');
+    return res.status(500).json({ message: 'Internal Server error', error });
   }
 };
