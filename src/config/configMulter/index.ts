033f1e303558c7c958f1dd28241d8a29bcddedab
RichardMadrigar <richard_madrigar99@hotmail.com>
2022-03-03 01:12:23 -0300

    add s3 upaload ok com banco ok

--- src/config/configMulter/index.ts
+++ src/config/configMulter/index.ts
@@ -3,11 +3,11 @@ import { Request } from 'express';
 import multer, { FileFilterCallback } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+import aws from 'aws-sdk';
+import MulterS3 from 'multer-s3';
 
-export const multerConfig = {
-  dest: path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'), // para onde os arquivos vao quando fizer o upload
-
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
 
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'));
@@ -15,12 +15,30 @@ export const multerConfig = {
     filename: (req, file, cb) => {
       const fileHash = crypto.randomBytes(16).toString('hex');
 
+      const key = `${fileHash}-${file.originalname}`;
+
+      cb(null, key);
+    },
+  }),
+  s3: MulterS3({
+    s3: new aws.S3(),
+    bucket: 'upload-img-aula',
+    contentType: MulterS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      const fileHash = crypto.randomBytes(16).toString('hex');
+
       const fileName = `${fileHash}-${file.originalname}`;
 
       cb(null, fileName);
     },
   }),
+};
 
+export const multerConfig = {
+  dest: path.resolve(__dirname, '..', '..', '..', 'tmp', 'uploads'), // para onde os arquivos vao quando fizer o upload
+
+  storage: storageTypes.s3, // local do armazenamento
   limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB =>  limite para os arquivos
 
   // filtrar tipos de arquivos
--- src/controllers/Upload.controller.ts
+++ src/controllers/Upload.controller.ts
@@ -2,24 +2,32 @@ import { Request, Response } from 'express';
 import { pool } from '../config/configDataBase/database';
 import logger from '../config/configLogger';
 
+interface IPropsFile {
+  originalname?: string;
+  size?: string;
+  key?: string;
+  location?: string;
+}
+
 export const upload = async (req: Request, res: Response) => {
-  const result = req.file;
+  const result: IPropsFile | any = req.file;
 
   if (!result) return res.status(400).json({ message: 'nao tem conteudo' });
 
-  const { originalname: name, size, filename: key } = result;
-  const url = '';
+  const {
+    originalname: name, size, key, location,
+  } = result;
 
   try {
     const SQL = `INSERT INTO permissao_usuarios ( name, size, key, url)
                                          VALUES ( $1, $2, $3, $4 )`;
-
-    const values = [name, size, key, url];
+    const values = [name, size, key, location];
 
     await pool.query(SQL, values);
 
-    logger.info('Foto guardada no BD com sucesso');
-    return res.status(200).json('Upload no diretorio OK');
+    const { rows } = await pool.query('SELECT * FROM  permissao_usuarios WHERE key = $1', [key]);
+
+    return res.status(200).json(rows);
   } catch (error) {
     logger.fatal(error);
     return res.status(500).json('Internal Server error');
